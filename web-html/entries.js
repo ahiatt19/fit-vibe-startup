4133f8928abcf05f22edeb2ff6eb604641c7f4ae
Aubry <ahiatt19@byu.edu>
2023-10-27 17:59:12 -0600

    entries are in the data table

--- web-html/entries.js
+++ web-html/entries.js
@@ -12,6 +12,7 @@ function loadEntries() {
     if (entriesText) {
         entries = JSON.parse(entriesText);
     }
+    //console.log(entries)
 
     const tableBodyE1 = document.querySelector('#entries');
     //console.log(tableBodyE1)
@@ -20,23 +21,26 @@ function loadEntries() {
         for (const [i, entry] of entries.entries()) {
             const rankTdE1 = document.createElement('td');
             const usernameTdE1 = document.createElement('td');
-            const calTdE1 = document.createElement('td');
-            const wrkoutTdE1 = document.createElement('td');
+            const caloriesTdE1 = document.createElement('td');
+            const workoutTdE1 = document.createElement('td');
             const notesTdE1 = document.createElement('td');
 
             rankTdE1.textContent = i + 1;
             usernameTdE1.textContent = entry.name;
-            calTdE1.textContent = entry.calories;
-            wrkoutTdE1.textContent = entry.workout;
+            caloriesTdE1.textContent = entry.calories;
+            workoutTdE1.textContent = entry.workout;
             notesTdE1.textContent = entry.note;
 
             const rowEl = document.createElement('tr');
+            //console.log(rowE1);
             rowEl.appendChild(rankTdE1);
             rowEl.appendChild(usernameTdE1);
-            rowEl.appendChild(wrkoutTdE1);
+            rowEl.appendChild(caloriesTdE1);
+            rowEl.appendChild(workoutTdE1);
             rowEl.appendChild(notesTdE1);
+            
 
-            tableBodyEl.appendChild(rowEl);
+            tableBodyE1.appendChild(rowEl);
         }
     } else {
         tableBodyE1.innerHTML = '<tr><td colSpan=5>Be the first to compete!</td></tr>';
